c87718fee86957447beb7eb245601bf50c8a732e
Bruno Capella <bru_o_gato@hotmail.com>
2021-03-04 21:19:44 -0300

    aulaprojeto004

--- src/app/pages/user-add/user-add.page.ts
+++ src/app/pages/user-add/user-add.page.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user';
 import { Storage } from '@ionic/storage';
-import { AlertController } from '@ionic/angular';
 import { UserServiceService } from '../../services/user-service.service';
-import { ToastController } from '@ionic/angular';
+import { MgService } from '../../services/mg.service';
 
 @Component({
   selector: 'app-user-add',
@@ -17,9 +16,9 @@ export class UserAddPage implements OnInit {
 
   constructor(
     private storage: Storage,
-    public alertController: AlertController,
-    private userService: UserServiceService,
-    public toastController: ToastController
+    // public alertController: AlertController,
+    private userService: UserServiceService,    // public toastController: ToastController,
+    protected msg:MgService
   ) { }
 
   ngOnInit() {
@@ -30,7 +29,7 @@ export class UserAddPage implements OnInit {
         res => {
           console.log(res);
           if (res.erro) {
-            this.presentToast("CEP não localizado!");
+            this.msg.presentToast("CEP não localizado!");
           } else {
             //this.user = res;
             //this.user.cep = res.cep;
@@ -62,32 +61,19 @@ export class UserAddPage implements OnInit {
       this.storage.set('email', this.user.email);
       this.storage.set('senha', this.user.senha);
       console.log('Dados Salvos...', this.user);
-      this.presentAlert();
+      this.msg.presentAlert('Alerta','Usuário Cadastrado.');
     } catch (error) {
       console.error("Erro ao salvar.", error);
+      this.msg.presentAlert("Erro","Não Foi possivel salvar.");
     }
 
   }
-
-
-  async presentAlert() {
-    const alert = await this.alertController.create({
-      cssClass: 'my-custom-class',
-      header: 'Alerta',
-      //subHeader: 'Subtitle',
-      message: 'Usuário cadastrado.',
-      buttons: ['OK']
-    });
-
-    await alert.present();
-  }
-
-  async presentToast(texto: string) {
-    const toast = await this.toastController.create({
-      message: texto,
-      duration: 2000
-    });
-    toast.present();
-  }
+  // //async presentToast(texto: string) {
+  //   const toast = await this.toastController.create({
+  //     message: texto,
+  //     duration: 2000
+  //   });
+  //   toast.present();
+  // }
 
 }
